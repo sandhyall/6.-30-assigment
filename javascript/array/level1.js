3a219c2465430fa666fac622f30488d52531bafa
sandhya_oic <sandhyadahal864@gmail.com>
2024-07-31 07:50:55 +0545

    added new question reverse

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -3,3 +3,7 @@ const arr = [3,4,5]
 // find the length of the array
 //expected output is 3
 console.log(arr.length)
+//Q2 
+//reverse the array
+//expected output is [5,4,3]
+console.log(arr.reverse())
